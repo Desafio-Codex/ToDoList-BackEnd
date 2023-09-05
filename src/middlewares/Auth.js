3628c485f2307524006fa73dec51af5cda6e39ae
Miguel Macedo Ferreira <mmmiguel_macedo@outlook.com>
2023-09-05 19:22:10 -0300

    Adicionando APIs e Pastas de Testes

--- src/middlewares/Auth.js
+++ src/middlewares/Auth.js
@@ -1,45 +1,18 @@
 const jwt = require('jsonwebtoken');
-const User = require('../model/User');
-require('dotenv/config');
 
-module.exports = {
-    authorizeUser(req, res, next) {
-        authorize(req, res, next, "user");
+const auth = (req, res, next) => {
+  const token_header = req.headers.auth;
+  if (!token_header) {
+    return res.status(401).send({ error: 'Token não enviado!' });
+  }
+
+  jwt.verify(token_header, 'batatafrita2019', (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: 'Token inválido!' });
     }
-}
+    res.locals.auth_data = decoded;
+    return next();
+  });
+};
 
-authorize = (req, res, next, type) => {
-    const authHeader = req.headers.authorization;
-
-    if (!authHeader)
-        return res.status(401).send({ error: 'Sem token irmão' });
-
-    const parts = authHeader.split(' ');
-
-    if (!parts.length === 2)
-        return res.status(401).send({ error: 'Erro de token' });
-
-    const [scheme, token] = parts;
-
-    if (!/^Bearer$/i.test(scheme))
-        return res.status(401).send({ error: 'Token mal formatado' });
-
-    jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
-        if (err)
-            return res.status(401).send({ error: 'Token inválido' });
-
-        const user = await User.findOne({ _id: decoded.sub });
-
-        if (!user)
-            return res.status(404).send({ error: 'Usuário não existe!' });
-
-        const token_list = user.token_list;
-        if (!token_list.includes(token)) {
-            return res.status(401).send({ error: 'Token inválido' });
-        }
-
-        req.id = decoded.sub;
-        req.user = user;
-        return next();
-    })
-}
+module.exports = auth;
---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../middlewares/auth');
+
+router.get('/', auth, (req, res) => {
+  return res.send({ message: 'Essa informação é muito importante. Usuários não autorizados não deveriam recebê-la!' });
+});
+
+router.post('/', (req, res) => {
+  return res.send({ message: 'Tudo ok com o método POST da raiz!' });
+});
+
+module.exports = router;
