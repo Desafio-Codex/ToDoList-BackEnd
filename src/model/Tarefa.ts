ee48d723920c88daf187faf894aa3370765384c8
Miguel Macedo Ferreira <111234286+Maccelerator@users.noreply.github.com>
2023-09-11 14:41:19 -0300

    Update Tarefa.ts
    Adição do model de Tarefas, escrito com typescript e mongoose.

--- src/model/Tarefa.ts
+++ src/model/Tarefa.ts
@@ -1,9 +1,45 @@
+import { Schema, model, Document } from 'mongoose';
+
+// Defina a interface que representa a estrutura dos dados da tarefa
+interface ITarefa extends Document {
+  nome: string;
+  descricao: string;
+}
+
+// Crie um esquema Mongoose para a Tarefa
+const TarefaSchema = new Schema<ITarefa>({
+  nome: String,
+  descricao: String,
+});
+
+// Defina a classe Tarefa com construtor, getters e setters
 class Tarefa {
-    nome: string;
-    descricao: string;
-  
-    constructor(nome: string, descricao: string) {
-      this.nome = nome;
-      this.descricao = descricao;
-    }
+  private _nome: string;
+  private _descricao: string;
+
+  constructor(nome: string, descricao: string) {
+    this._nome = nome;
+    this._descricao = descricao;
   }
+
+  get nome(): string {
+    return this._nome;
+  }
+
+  set nome(novoNome: string) {
+    this._nome = novoNome;
+  }
+
+  get descricao(): string {
+    return this._descricao;
+  }
+
+  set descricao(novaDescricao: string) {
+    this._descricao = novaDescricao;
+  }
+}
+
+// Crie o modelo Mongoose para a Tarefa
+const TarefaModel = model<ITarefa>('Tarefa', TarefaSchema);
+
+export { Tarefa, TarefaModel };
